1aef6200ef381335691e8872c749e306c9f88c52
Mannan Qazi <mannanqazi2@gmail.com>
2018-10-26 16:58:29 -0600

    Added smooth-scrolling and separated the JS file

---
+++ bs4boiler/js/main.js
@@ -0,0 +1,21 @@
+$('#year').text(new Date().getFullYear());
+
+// Init ScrollSpy
+
+$('body').scrollspy({target: '#main-nav'});
+
+// Add ScrollSpy
+
+$('#main-nav a').on('click', function (event) {
+    if(this.hash !== " "){
+        event.preventDefault();
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function () {
+
+            window.location.hash = hash;
+        });
+    }
+});
